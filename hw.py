7286ddd41a392c8fa95b20443705bf52521e6298
Chiffa <babiy99021422@gmail.com>
2023-04-29 03:33:41 +0300

    Сделал задание 28

--- hw.py
+++ hw.py
@@ -1,24 +1,21 @@
-# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+# Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
 # *Пример:*
 
-# A = 3; B = 5 -> 243 (3⁵)
-#     A = 2; B = 3 -> 8 
+# 2 2
+#     4 
 
-
-def power(base, exp):
-    if exp == 0:
-        return 1
-    elif exp % 2 == 0:
-        temp = power(base, exp//2)
-        return temp * temp
+def sum(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
     else:
-        temp = power(base, (exp-1)//2)
-        return base * temp * temp
+        return sum(a-1, b-1) +1+1
 
-num = int(input("Введите число: "))
-power_val = int(input("Введите степень: "))
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
 
-result = power(num, power_val)
+result = sum(a, b)
 
-print(f"A = {num}; B = {power_val} -> {result}")
+print(a, " + ", b, " = ", result)
